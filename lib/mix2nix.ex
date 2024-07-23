5185b948af4af367b14706301c054123e89def18
Ryan Rasti <ryanrasti@protonmail.com>
2024-07-23 11:50:12 -0700

    fix issue with name vs. namestr

--- lib/mix2nix.ex
+++ lib/mix2nix.ex
@@ -90,14 +90,14 @@ defmodule Mix2nix do
 	end
 
 	def nix_expression(
-		allpkgs, _name_str,
+		allpkgs, name_str,
 		{:hex, name, version, _hash, builders, deps, repo, hash2}
-	), do: get_hexpm_expression(allpkgs, name, version, builders, deps, repo, hash2)
+	), do: get_hexpm_expression(allpkgs, name, name_str, version, builders, deps, repo, hash2)
 
 	def nix_expression(
-		allpkgs, _name_str,
+		allpkgs, name_str,
 		{:hex, name, version, _hash, builders, deps, repo}
-	), do: get_hexpm_expression(allpkgs, name, version, builders, deps, repo)
+	), do: get_hexpm_expression(allpkgs, name, name_str, version, builders, deps, repo)
 
     def nix_expression(_allpkgs, name_str, {:git, url, rev, []}) do
 		"""
@@ -121,19 +121,22 @@ defmodule Mix2nix do
 		""
 	end
 
-	defp get_hexpm_expression(allpkgs, name, version, builders, deps, repo, sha256 \\ nil) do
+	defp get_hexpm_expression(allpkgs, name, name_str, version, builders, deps, repo, sha256 \\ nil) do
+		# `name_str` appears to be the common name and `name` is the value needed to fetch the package.
+		# Usually they are the same, but some packages (e.g., https://hex.pm/packages/ts_chatterbox) use
+		# a different name for fetching.
 		name = Atom.to_string(name)
 		buildEnv = get_build_env(builders, name)
 		sha256 = sha256 || get_hash(name, version)
 		deps = dep_string(allpkgs, deps)
 
 		"""
-		    #{name} = #{buildEnv} rec {
-		      name = "#{name}";
+		    #{name_str} = #{buildEnv} rec {
+		      name = "#{name_str}";
 		      version = "#{version}";
 
 		      src = fetchHex {
-		        pkg = "${name}";
+		        pkg = "#{name}";
 		        version = "${version}";
 		        sha256 = "#{sha256}";
 		        repo = "#{repo}";
